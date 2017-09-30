644d07c5aa46201daf0759cfe3ffb0c6a9123b43
Tuiaa <minjasilvennoinen@gmail.com>
2017-09-30 19:15:33 +0300

    Added background scrolling logic

--- DefenderRemake/Assets/Scripts/PlayerMovement.cs
+++ DefenderRemake/Assets/Scripts/PlayerMovement.cs
@@ -5,19 +5,46 @@ using UnityEngine;
 public class PlayerMovement : MonoBehaviour {
 
     [SerializeField]
-    private float _playerShipSpeed = 5;
+    private float _playerShipSpeedHorizontal = 25;
+    [SerializeField]
+    private float _playerShipSpeedVertical = 15;
+    [SerializeField]
+    private GameObject _background;
+
+    private bool _goingToRight = false;
+    private bool _goingToLeft = true;
 
     private void FixedUpdate()
     {
-        if(Input.GetKey(KeyCode.LeftArrow))
+        if(_goingToLeft)
+        {
+            _background.transform.position += Vector3.right * _playerShipSpeedHorizontal * Time.deltaTime;
+        }
+
+        if (_goingToRight)
+        {
+            _background.transform.position += Vector3.left * _playerShipSpeedHorizontal * Time.deltaTime;
+        }
+
+        if (Input.GetKey(KeyCode.LeftArrow))
         {
             transform.localScale = Vector3.one;
-            transform.position += Vector3.left * _playerShipSpeed * Time.deltaTime;
+            _goingToLeft = true;
+            _goingToRight = false;
         }
         if(Input.GetKey(KeyCode.RightArrow))
         {
             transform.localScale = new Vector3(-1, 1, 1);
-            transform.position += Vector3.right * _playerShipSpeed * Time.deltaTime;
+            _goingToRight = true;
+            _goingToLeft = false;
+        }
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += Vector3.up * _playerShipSpeedVertical * Time.deltaTime;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += Vector3.down * _playerShipSpeedVertical * Time.deltaTime;
         }
     }
 }
---
+++ DefenderRemake/Assets/Scripts/ScrollingBackground.cs
@@ -0,0 +1,75 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScrollingBackground : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _parentBackground;
+
+    private Transform[] _backgrounds;
+    private int _childCount = 0;
+
+    private int _leftIndex;
+    private int _rightIndex;
+    private int _backgroundOffset = 25;
+
+    private void Start()
+    {
+        InitializeList();
+    }
+
+    private void Update()
+    {
+        if(Input.GetKeyUp(KeyCode.A))
+        {
+            ScrollLeft();
+        }
+
+        if (Input.GetKeyUp(KeyCode.D))
+        {
+            ScrollRight();
+        }
+    }
+
+    private void InitializeList()
+    {
+        _backgrounds = new Transform[transform.childCount];
+        foreach (Transform child in transform)
+        {
+            _backgrounds[_childCount] = child;
+            _childCount++;
+        }
+
+        _leftIndex = 0;
+        _rightIndex = _childCount - 1;
+    }
+
+    private void ScrollLeft()
+    {
+        int previousRight = _rightIndex;
+        float newPosition = _backgrounds[_leftIndex].position.x - _backgroundOffset;
+        _backgrounds[_rightIndex].position = new Vector3(newPosition, 0, 0);
+
+        _leftIndex = _rightIndex;
+        _rightIndex--;
+        if(_rightIndex < 0)
+        {
+            _rightIndex = _childCount - 1;
+        }
+    }
+
+    private void ScrollRight()
+    {
+        int previousLeft = _leftIndex;
+        float newPosition = _backgrounds[_rightIndex].position.x + _backgroundOffset;
+        _backgrounds[_leftIndex].position = new Vector3(newPosition, 0, 0);
+
+        _rightIndex = _leftIndex;
+        _leftIndex++;
+        if (_leftIndex == _backgrounds.Length)
+        {
+            _leftIndex = 0;
+        }
+    }
+}
