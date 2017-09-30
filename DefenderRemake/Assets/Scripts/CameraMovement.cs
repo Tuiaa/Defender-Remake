0387120a8958853cc1a9001281881e93327a77a5
Tuiaa <minjasilvennoinen@gmail.com>
2017-09-30 14:30:01 +0300

    PlayerMovement added

---
+++ DefenderRemake/Assets/Scripts/CameraMovement.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMovement : MonoBehaviour {
+
+    [SerializeField]
+    private GameObject _player;
+
+    private void Update()
+    {
+        float x = _player.transform.position.x;
+
+        transform.position = new Vector3(x, transform.position.y, transform.position.z);
+    }
+}
---
+++ DefenderRemake/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+    [SerializeField]
+    private float _playerShipSpeed = 5;
+
+    private void FixedUpdate()
+    {
+        if(Input.GetKey(KeyCode.LeftArrow))
+        {
+            transform.localScale = Vector3.one;
+            transform.position += Vector3.left * _playerShipSpeed * Time.deltaTime;
+        }
+        if(Input.GetKey(KeyCode.RightArrow))
+        {
+            transform.localScale = new Vector3(-1, 1, 1);
+            transform.position += Vector3.right * _playerShipSpeed * Time.deltaTime;
+        }
+    }
+}
